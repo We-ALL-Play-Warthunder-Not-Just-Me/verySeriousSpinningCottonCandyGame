073b0a94611ef7a35044fb4437cead1486e67cd0
Gadzik <oshawot123@gmail.com>
2026-06-22 00:34:28 -0500

    Player Enhancements
    Finished Player movements more or less for now and added in their animations in relation to their health/spin force. Also, Enemies are now Rigidbodies. Yep.

--- Player/player_rigid.gd
+++ Player/player_rigid.gd
@@ -1,33 +1,49 @@
 extends RigidBody2D
 
 var mouse_position
-var amplifier = 100
-@onready var arrow_test = $VerySeriousArrows3
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
+var amplifier = 350
+var max_power = 60
+@onready var draw_arrow = $VerySeriousArrows3
+@onready var red_arrow = load("res://Assets/Images/Very Serious Arrows1.png")
+@onready var yellow_arrow = load("res://Assets/Images/Very Serious Arrows2.png")
+@onready var green_arrow = load("res://Assets/Images/Very Serious Arrows3.png")
+@onready var health = $HealthComponent
+@onready var animations = $VerySeriousPlayer/PlayerAnimations
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("MouseLeftClick"):
+		self.set_linear_velocity(Vector2(0,0))
+
 	if Input.is_action_pressed("MouseLeftClick"):
-		arrow_test.visible = true
+		draw_arrow.visible = true
 		mouse_position = get_global_mouse_position()
-		print("Hold!")
-		print("Mouse Position: ", mouse_position)
-		var test_position_x
-		var test_position_y
-		test_position_x = self.position.x - mouse_position.x
-		test_position_y = self.position.y - mouse_position.y
-		arrow_test.position = Vector2(test_position_x, test_position_y).limit_length(60)
-		arrow_test.rotation = arrow_test.position.angle()
+		var draw_arrow_limit = self.position - mouse_position
+		draw_arrow.position = draw_arrow_limit.limit_length(max_power)
+		draw_arrow.rotation = draw_arrow.position.angle()
+		if draw_arrow_limit.length() > 30:
+			draw_arrow.texture = green_arrow
+		elif draw_arrow_limit.length() > 15:
+			draw_arrow.texture = yellow_arrow
+		elif draw_arrow_limit.length() > 0:
+			draw_arrow.texture = red_arrow
+		#print("Hold!")
+		#print("Mouse Position: ", mouse_position)
 		
-	
 	if Input.is_action_just_released("MouseLeftClick"):
 		print("Send!")
-		arrow_test.visible = false
-		var x_force = (self.position.x - mouse_position.x) * amplifier
-		var y_force = (self.position.y - mouse_position.y) * amplifier
-		var total_force = Vector2(x_force, y_force)
-		self.apply_force(total_force)
+		draw_arrow.visible = false
+		var force = (self.position - mouse_position)
+		var lim_force = force.limit_length(max_power)
+		self.apply_force(lim_force * amplifier)
+		#print(force)
+		#print(lim_force)
+	
+	if health.CurrentHP > 50:
+		animations.play("PlayerSpinHigh")
+	elif health.CurrentHP > 25:
+		animations.play("PlayerSpinMed")
+	elif health.CurrentHP > 0:
+		animations.play("PlayerSpinLow")
+	else:
+		animations.stop()
