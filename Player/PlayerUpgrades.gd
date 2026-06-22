3d0adf474fb1ca08170da2d8338993d2ae7e71bf
Snowyfreezer <99560297+Snowyfreezer@users.noreply.github.com>
2026-06-21 23:19:41 -0400

    created custom resource playerUpgrades to keep track of player upgrades

---
+++ Player/PlayerUpgrades.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name playerUpgrades
+
+# So keep track of upgrades by it's lvl ie if the upgrade is lvl 1 or 2 or 3
+
+@export var SpinAttack: int = 0
+@export var MaxSpinPower: int = 0
+@export var ReduceSpinDecayRate: int = 0
+@export var CottonCandyStealPower: int = 0
+@export var StrongerEnemies: int = 0
+
